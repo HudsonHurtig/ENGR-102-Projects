a02442f5b4b0b0533d5ff2eefc4fe488c3caa39d
Hudsonhurtig@yahoo.com <johndough1432@gmail.com>
2022-09-02 14:32:14 -0500

    finished linear interpolation

--- linear_interpolation.py
+++ linear_interpolation.py
@@ -1,19 +1,25 @@
 # By submitting this assignment, I agree to the following:
 #   "Aggies do not lie, cheat, or steal, or tolerate those who do."
 #   "I have not given or received any unauthorized aid on this assignment."
-# Name:         Hudson Hurtig
-# Section:      505
-# Assignment:   lab 1
-# Date:         8-30-22
 #
+# Names:        Hudson Hurtig
+#               Avi Punjabi
+#               Brian Tran
+#               MJ Murray
+#               Gabriel Isaac
+# Section:      505
+# Assignment:   Lab 2
+# Date:         9-2-2022
 
-#comment
+from math import *
+#part 1
+time1 = 25 
 
-print('''\nPart 1:
-For t = 25 minutes, the position p = 9026.0 kilometers
-Part 2:
-For t = 300 minutes, the position p = 10219.078642554414 kilometers\n''')
+print("Part 1:")
+print("For t =",time1,"minutes, the position p =",round(((time1*((23026-2026)/45)) - (2640+(2/3))),1),"kilometers")
 
-#comment
+#part 2
+time2 = 300
 
-#im not entirely sure what this assignment is asking of us
+print("Part 2:")
+print("For t = ",time2," minutes, the position p =",((time2*((23026-2026)/45)) - (2640+(2/3))) % (2*pi*6745),"kilometers")
