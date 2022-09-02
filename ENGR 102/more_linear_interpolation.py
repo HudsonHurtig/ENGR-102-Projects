f82d00872cce7c783b5c61357ec53fbca52682aa
Hudsonhurtig@yahoo.com <johndough1432@gmail.com>
2022-09-02 16:45:45 -0500

    calculations work for point interpolation

--- ENGR 102/more_linear_interpolation.py
+++ ENGR 102/more_linear_interpolation.py
@@ -18,19 +18,21 @@ class Point:
         self.time = time
         
         self.arr = [x,y,z,time]
+   
     
 class movingBody:
     
-    def __init__(self,vX,vY,vZ):
+    def __init__(self,vX,vY,vZ,initX,initY,initZ):
         
         self.vX = vX
         self.vY = vY
         self.vZ = vZ
         
-        self.velocityArray = [vX,vY,vZ]
+        self.initX = initX
+        self.initY = initY
+        self.initZ = initZ
         
-        
-        
+        self.velocityArray = [vX,vY,vZ]         
 
 
 def interpolatePoints(a, b):
@@ -41,33 +43,47 @@ def interpolatePoints(a, b):
     vY = (b.y-a.y)/tdiff
     vZ = (b.z-a.z)/tdiff
     
-    return movingBody(vX, vY, vZ)
-
+    initX = a.x - vX * a.time
+    initY = a.y - vY * a.time
+    initZ = a.z - vZ * a.time 
+    
+    print(initX)
+    return movingBody(vX, vY, vZ, initX, initY, initZ)
 
 
 def locateBody(body, time):
     
-    bodyX = body.vX * time
-    bodyY = body.vY * time
-    bodyZ = body.vZ * time
+    bodyX = body.vX * time + body.initX
+    bodyY = body.vY * time + body.initY
+    bodyZ = body.vZ * time + body.initZ
     
     return Point(bodyX, bodyY, bodyZ, time)
-    
-    
-    
+
+
+      
 
 # point 1
 
-point1 = Point(8,6,7,12)
+givenPoint1 = Point(8,6,7,12)
+
 
 # point 2
 
-point2 = Point(-5,30,9,85)
+givenPoint2 = Point(-5,30,9,85)
 
 
-# creates a moving body
+# creates a moving body through point interpolation
 
-body = interpolatePoints(point1, point2)
+body = interpolatePoints(givenPoint1, givenPoint2)
+
+#locates moving body at time
+
+point3 = locateBody(body, 30.0)
+
+print("At time",point3.time,"seconds:") 
+print("x1 =",point3.x,"m")
+print("y1 =",point3.y,"m")
+print("z1 =",point3.z,"m")
 
 
 
