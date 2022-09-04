bc52d10f6e43fcb0e05758a5524ac47a06951b30
Hudsonhurtig@yahoo.com <johndough1432@gmail.com>
2022-09-04 11:58:58 -0500

    I think this will work

--- ENGR 102/python_code_soup.py
+++ ENGR 102/python_code_soup.py
@@ -1,16 +1,225 @@
+# By submitting this assignment, I agree to the following:
+#   "Aggies do not lie, cheat, or steal, or tolerate those who do."
+#   "I have not given or received any unauthorized aid on this assignment."
+#
+# Name:         Hudson
+# Section:      506
+# Assignment:   Lab topic 2 individual
+# Date:         9/2/22
+
 x = 1 
-y = 10 
 z = 0 
+z += x #z = 1
+print(z)
+z = 0
+
+
+y = 10 
+z += y 
+z += y 
+x += 1 
+x += 1 
+x += 1 
+x += 1
+x += 1 
+y *= x 
+z += x
+print(z)
+
+y = 10 
+z += y 
+z += y
+z += y 
+z += y
+z += y
+z += y 
+z += y
+
+x = 1 
+z += x
+z += x
+z += x
+z += x
+z += x
+z += x
+
+print(z)
+
+x = 1 
+z = 0 
+y = 10 
+x += 1 
+x += 1 
+x += 1 
+x += 1
+x += 1
+x += 1 
+x += 1 
+x += 1 
+x += 1 # x = 10 y = 10
+
+y *= x 
+y *= x
+y *= x
+y *= x
+y *= x
+y *= x
+y *= x
+y *= x
+
+
+z += y 
+
+print(z)
+
+x = 1 
+z = 0 
+y = 10 
+
+x = 1 
+z = 0 
+y = 10 
+x += 1 
+x += 1 
+x += 1 
+x += 1
+x += 1
+x += 1 
+x += 1 
+x += 1 
+x += 1 # x = 10 y = 10
+
+y *= x 
+
+z += y 
+z += y 
+z += y 
+z += y 
+z += y 
+z += y 
+z += y 
+z += y 
+z += y 
+
+z += y 
+z += y 
+z += y 
+z += y 
+z += y 
+z += y 
+z += y 
+z += y 
+z += y 
+
+z += y 
+z += y 
+z += y 
+z += y 
+z += y 
+z += y 
+z += y 
+z += y 
+z += y 
+
+z += y 
+z += y 
+z += y 
+z += y 
+z += y 
+z += y 
+z += y 
+z += y 
+z += y 
+
+z += y 
+z += y 
+z += y 
+z += y 
+z += y 
+z += y 
+z += y 
+z += y 
+z += y 
+
+z += y 
+z += y 
+z += y 
+z += y 
+z += y 
+z += y 
+z += y 
+z += y 
+z += y 
+
+z += y 
+z += y 
+z += y 
+z += y 
+z += y 
+z += y 
+z += y 
+z += y 
+z += y 
+
+z += y 
+z += y 
+z += y 
+z += y 
+z += y 
+z += y 
+z += y 
+z += y 
+z += y 
+
+z += y 
+z += y 
+z += y 
+z += y 
+z += y 
+z += y 
+z += y 
+z += y 
+z += y 
+
+z += y 
+z += y 
+z += y 
+z += y 
+z += y 
+
+y = 10 
+
+z += y 
+z += y 
+z += y 
+z += y 
+z += y 
+z += y 
+z += y 
+
+
+x = 1 
+
+z += x 
+z += x 
+z += x 
+z += x 
+z += x 
+
+print(z)
+
+
+
 x = y 
 x += 1 
 y += x 
 y *= x 
 z += x 
 z += y 
-print(z) 
 
 
-print(z)
+
+
 # 1 
 # 26 
 # 102 
