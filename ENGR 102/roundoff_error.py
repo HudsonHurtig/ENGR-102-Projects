cb5647a84303eceb55b9ed9ba1e6ff4bab1e1afc
Hudsonhurtig@yahoo.com <johndough1432@gmail.com>
2022-09-19 00:39:32 -0500

    roundoff error works and got full credit

--- ENGR 102/roundoff_error.py
+++ ENGR 102/roundoff_error.py
@@ -26,36 +26,49 @@ print(f'f = 2 * a + 5 * a = {f}')
 # no it is not 1
 
 from math import sqrt 
-x = sqrt(1 / 3)    
-print(f'x = {x}') 
-y = x * x * 3     
-print(f'y = x * x * 3 = {y}') 
-z = x * 3 * x     
+
+x = sqrt(1 / 3)
+print(f'x = {x}')
+y = x * x * 3
+print(f'y = x * x * 3 = {y}')
+z = x * 3 * x
 print(f'z = x * 3 * x = {z}')
 
-TOL = 1e-10 
 
-if abs(y - z) < TOL: 
-    
-    print(f'y and z are equal within tolerance of {TOL}') 
-    
-else:                 
-    
-    print(f'y and z are NOT equal within tolerance of {TOL}') 
 
 # no it is not 1
 
 ############ Part B ############
 
-TOL = 1e-10 
-
-if abs(b - f) < TOL: 
+TOL = 1e-10
+if abs(b - f) < TOL:
     
-    print(f'b and f are equal within tolerance of {TOL}') 
+    print(f'b and f are equal within tolerance of {TOL}')
     
 else:                 
     
-    print(f'b and f are NOT equal within tolerance of {TOL}') 
+    print(f'b and f are NOT equal within tolerance of {TOL}')
+
+TOL = 1e-10
+
+if abs(y - z) < TOL:
+    
+    print(f'y and z are equal within tolerance of {TOL}')
+    
+else:  
+    
+    print(f'y and z are NOT equal within tolerance of {TOL}')
+
+############ Part C ############
+ 
+m = 0.1    
+print(f'm = {m}')
+n = 3 * m
+print(f'n = 3 * m = 0.3 {n==0.3}')
+p = 7 * m
+print(f'p = 7 * m = 0.7 {p==0.7}')
+q = n + p
+print(f'q = n + p = 1 {q==1}')
     
 
     
