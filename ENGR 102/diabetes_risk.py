f332b511db04ce223e281dcd290ac2fa1f283623
Hudsonhurtig@yahoo.com <johndough1432@gmail.com>
2022-09-22 09:35:33 -0500

    avi finished diabetes calc

--- ENGR 102/diabetes_risk.py
+++ ENGR 102/diabetes_risk.py
@@ -1,52 +1,89 @@
-import math
+# -*- coding: utf-8 -*-
+# By submitting this assignment, I agree to the following:
+#   "Aggies do not lie, cheat, or steal, or tolerate those who do."
+#   "I have not given or received any unauthorized aid on this assignment."
+#
+# Names:        Avi Punjabi
+#               MJ Murray
+#               Hudson Hurtig
+#               Gabriel Isaac
+#               Brian Tran
+# Section:      575
+# Assignment:   Lab 5.3.1
+# Date:         09 20 22
+from math import *
 
-def normalizeSex(sex):
-    #if sex is female assign the weight .879 else assign 0
-    if sex == 'F':
-        
-        weight = .879
-        
+sexNum = 0
+ageNum = 0
+BMINum = 0
+HMNum = 0
+steroidsNum = 0
+smokeNum = 0
+histNum = 0
+
+sexInput = input("Enter your sex (M/F): ") 
+
+if sexInput == 'F':
+    sexNum = 0.879
+else:
+    sexNum = 0
+
+ageInput = float(input("Enter your age (years): ")) 
+
+ageNum = ageInput
+
+BMIInput = float(input("Enter your BMI: "))
+
+if BMIInput < 25:
+    BMINum = 0
+elif BMIInput >= 25 and BMIInput < 27.5:
+    BMINum = 0.699
+elif BMIInput >= 27.5 and BMIInput < 30:
+    BMINum = 1.97
+else:
+    BMINum = 2.518
+
+HMInput = input("Are you on medication for hypertension (Y/N)? ") 
+
+if HMInput == 'Y':
+    HMNum = 1.222
+else:
+    HMNum = 0
+
+steroidsInput = input("Are you on steroids (Y/N)? ") 
+
+if steroidsInput == 'Y':
+    steroidsNum = 2.191
+else:
+    steroidsNum = 0
+
+smokeInput = input("Do you smoke cigarettes (Y/N)? ") 
+
+if smokeInput == 'Y':
+    smokeNum = 0.855
+else:
+    smokeInputNew = input("Did you smoke in the past (Y/N)? ")
+
+    if smokeInputNew == 'Y':
+        smokeNum = -0.218
     else:
-        
-        weight = 0
-        
-    return weight
+        smokeNum = 0
 
-def normalizeAge(age):
-    # age is just a raw number in the calculation so return age
-    return age
+histInput = input("Do you have a family history of diabetes (Y/N)? ")
 
-def normalizeBMI(bmi):
-    #based on age it assigns a weight to return when the function is called 
-    if bmi < 25:
-        
-        weight = 0.0
-        
-    elif bmi >= 25 and bmi <= 27.49:
-        
-        weight = 0.699
-        
-    elif bmi >= 27.5 and bmi <= 29.99:
-        
-        weight = 1.97
-        
-    elif bmi >= 30.0:
-        
-        weight = 2.518
-        
-    return weight
+if histInput == 'Y':
+    histInputNew = input("Is it both a parent and a sibling (Y/N)? ")
+
+    if histInputNew == 'Y':
+        histNum = 0.753
+    else:
+        histNum = 0.728
+else:
+    histNum = 0
+
+n = 6.322 + sexNum - (0.063 * ageNum) - BMINum - HMNum - steroidsNum - smokeNum - histNum
+
+risk = 100 / (1 + exp(n))
 
 
-
-
-
-
-sexInput = input("Enter your sex (M/F):") 
-ageInput = input("Enter your age (years):") 
-BMIInput = input("Enter your BMI: 26")
-hypertensionInput = input("Are you on medication for hypertension (Y/N)?") 
-steroidsInput = input("Are you on steroids (Y/N)?") 
-cigarettesInput = input("Do you smoke cigarettes (Y/N)?") 
-smokePastInput = input("Did you used to smoke (Y/N)?") 
-diabetesInput = input("Do you have a family history of diabetes (Y/N)?") 
-
+print(f'Your risk of developing type-2 diabetes is {risk:.2f}%')
