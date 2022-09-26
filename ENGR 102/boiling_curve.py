aa475bb1ad601ddf0601a51a4bb3969b497ef2ce
Hudsonhurtig@yahoo.com <johndough1432@gmail.com>
2022-09-26 11:15:29 -0500

    added piece wise unions function

--- ENGR 102/boiling_curve.py
+++ ENGR 102/boiling_curve.py
@@ -21,16 +21,16 @@ class Curve:
         self.lb = x0
         self.ub = x1
         
-        m = math.log(y1/y0)/math.log(x1/x0)
+        self.m = math.log(y1/y0)/math.log(x1/x0)
         
-        self.arr = [self.lb,self.ub,x0,x1,y0,y1,m]
+        self.arr = [self.lb,self.ub,x0,x1,y0,y1,self.m]
     
-    def getPointAt(x):
+    def getPointAt(self, x):
         
         
         return self.y0 * pow(x/self.x0, self.m)
     
-    def getBounds():
+    def getBounds(self):
         
         return [self.lb, self.ub]
  
@@ -46,29 +46,70 @@ class PieceWise:
         for i in arg:
             l1.append(i)
         self.equations = l1
+        self.unions = []
         
-        for i in arg:
-            q = [i.lb,i.ub]
-            l2.append(q)
-        self.bounds = l2
+    def addUnions(self, x, y):
         
-    def getVal(x):
-            
-        for i in len(self.l2):
-            if self.l2[0] < x < self.l2[1]:
-                return "yes"
+        un = self.unions
         
-    
+        un.append([x,y])
         
-    
-    
-            
+        self.unions = un
+        
+    def getUnions(self):
+        
+        return self.unions
+        
 
-piec = PieceWise(Curve(1, 2, 3, 4), Curve(2, 3, 5, 5))
-    
-print(piec.getVal(1.4))
+    def getValue(self, x):
+        
+        l1 = self.equations
+        unions = self.getUnions()
+        
+        for q in range(len(l1)-1):
+            
+            bounds = l1[q].getBounds()
+            
+            
+            for i in unions:
+                
+                if i[0] == x:
+                    return i[1]
+                
+           
+            if bounds[0] < x < bounds[1]:
+                
+                return l1[q].getPointAt(x)
+            
+        
+                
+            
+        
+
         
     
+        
+c1 =   Curve(1, 2, 3, 4)
     
+c2 = Curve(2, 3, 5, 5)      
+
+piec = PieceWise(c1, c2, c2)
+
+
+
+piec.addUnions(1.3, 1000)  
+piec.addUnions(5, 7000)  
+piec.addUnions(30, 150000)  
+piec.addUnions(120, 25000)  
+piec.addUnions(1200, 1500000) 
+
+print(piec.getValue(5))
+
+
+    
+
+
+    
+
         
     
