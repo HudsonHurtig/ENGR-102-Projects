a4e07fb3ab52e81164d9063a5d36ef7fe442e9ce
Hudsonhurtig@yahoo.com <johndough1432@gmail.com>
2022-10-06 15:36:17 -0500

    loving 102

---
+++ ENGR 102/kaprekars_constant.py
@@ -0,0 +1,100 @@
+
+# By submitting this assignment, I agree to the following:
+#   "Aggies do not lie, cheat, or steal, or tolerate those who do."
+#   "I have not given or received any unauthorized aid on this assignment."
+# Name:         Hudson Hurtig
+# Section:      575
+# Assignment:   lab 6
+# Date:         9-22-22
+
+#silly whittle comment
+
+
+import math
+
+def errorMessage():
+    print("ERROR")
+    
+    
+def normalizeVarible(q):
+    
+    if type(q) != str():
+        q = str(q)
+        
+    l = list(q)
+    while len(l) < 4:
+        l.insert(0,'0')
+
+        
+    return l
+
+def isPlayable(q):
+    
+    return not(all([q[0] == q[i] for i in range(len(q))]))
+
+def toInt(l):
+    
+    return int("".join(l))
+
+
+def findK(l):
+    
+    v1 = [int(l[q]) for q in range(len(l))]
+    v1.sort()
+    v2 = v1[::-1]
+    
+    v1 = [str(v1[q]) for q in range(len(v1))]
+    v2 = [str(v2[q]) for q in range(len(v2))]
+    
+    v1 = toInt(v1)
+    v2 = toInt(v2)
+    
+    return abs(v1-v2)
+    
+    
+    
+o = int(input("Enter a four-digit integer:"))
+
+in1 = normalizeVarible(o)
+
+if isPlayable(in1):
+    k = 0
+    counter = 0
+    p = []
+    while k != 6174:
+          counter += 1
+    
+          k = findK(in1)
+          in1 = normalizeVarible(k)
+          p.append(k)
+          
+    
+    p = [str(p[q]) for q in range(len(p))]
+
+    print(o,">"," > ".join(p)) 
+    
+    print(f"{o} reaches 6174 via Kaprekar's routine in {counter} iterations")
+    
+else:
+    
+    k = 0
+    counter = 0
+    p = []
+    while k != 0:
+          counter += 1
+    
+          k = findK(in1)
+          in1 = normalizeVarible(k)
+          p.append(k)
+          
+    
+    p = [str(p[q]) for q in range(len(p))]
+
+    print(o,">","0") 
+    
+    print(f"{o} reaches 0 via Kaprekar's routine in 1 iterations")
+    
+    
+        
+    
+
