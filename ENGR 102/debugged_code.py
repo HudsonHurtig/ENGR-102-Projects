bc83063b91fb161b36b6d9ca82b50aef623d8a0d
Hudsonhurtig@yahoo.com <johndough1432@gmail.com>
2022-11-01 17:43:55 -0500

    added all the stuff

--- ENGR 102/debugged_code.py
+++ ENGR 102/debugged_code.py
@@ -6,15 +6,19 @@
 # Section:      575
 # Assignment:   lab 10
 # Date:         11/1/22
+
 vl = 0
-vh = 0
 ul = 0
-ll = 0
-def interpolate(x1, x2, y1, y2, x):
+hl = 0
+sl = 0
+
+
+
+def interpolate(x1, y1, x2, y2, x):
     '''This function interpolates between two points'''
     slope = (y2 - y1) / (x2 - x1)
     y = slope * (x - x1) + y1
-    return slope
+    return y
 ##### 5 MPa Data #####
 # temperature in degrees C
 temp5 = [0, 20, 40, 60, 80, 100, 120, 140, 160, 180, 200, 220, 240, 260]
@@ -39,7 +43,7 @@ h10 = [10.07, 93.28, 176.37, 259.55, 342.94, 426.62, 510.73, 595.45,681.01, 767.
 s10 = [0.0003, 0.2943, 0.5685, 0.826, 1.0691, 1.2996, 1.5191, 1.7293,1.9316, 2.1271, 2.3174, 2.5037, 2.6876, 2.871]
 ##### do calculations #####
 # get temperature and pressure from user
-temp = int(input("Enter a temperature between 0 and 260 deg C: "))
+temp = float(input("Enter a temperature between 0 and 260 deg C: "))
 pres = float(input("Enter a pressure between 5 and 10 MPa: "))
 # loop over temperatures in list to find the two that bound
 # the temperature entered by the user
@@ -51,32 +55,28 @@ for i in range(len(temp5)):
         sl, sh = s5[i], s10[i]
         break
     elif temp == temp5[i + 1]:  # if equal to upper bound
-        ul, uh = v5[i + 1], v10[i + 1]
-        vl, vh = u5[i + 1], u10[i + 1]
+        ul, uh = u5[i + 1], u10[i + 1] 
+        vl, vh = v5[i + 1], v10[i + 1]
         hl, hh = h5[i + 1], h10[i + 1]
         sl, sh = s5[i + 1], s10[i + 1]
         break
     elif temp < temp5[i + 1]:   # if in between bounds, interpolate
         vl = interpolate(temp5[i], v5[i], temp5[i + 1], v5[i + 1], temp)
-        vh = interpolate(temp10[i], v10[i], temp10[i + 1], v10[i + 1], 
-        temp)
+        vh = interpolate(temp10[i], v10[i], temp10[i + 1], v10[i + 1], temp)
         ul = interpolate(temp5[i], u5[i], temp5[i + 1], u5[i + 1], temp)
-        uh = interpolate(temp10[i], u10[i], temp10[i + 1], u10[i + 1], 
-        temp)
+        uh = interpolate(temp10[i], u10[i], temp10[i + 1], u10[i + 1], temp)
         hl = interpolate(temp5[i], h5[i], temp5[i + 1], h5[i + 1], temp)
-        hh = interpolate(temp10[i], h10[i], temp10[i + 1], h10[i + 1], 
-        temp)
+        hh = interpolate(temp10[i], h10[i], temp10[i + 1], h10[i + 1], temp)
         sl = interpolate(temp5[i], s5[i], temp5[i + 1], s5[i + 1], temp)
-        sh = interpolate(temp10[i], s10[i], temp10[i + 1], s10[i + 1], 
-        temp)
+        sh = interpolate(temp10[i], s10[i], temp10[i + 1], s10[i + 1], temp)
     break
 # now interpolate with pressure
 v = interpolate(5, vl, 10, vh, pres)
 u = interpolate(5, ul, 10, uh, pres)
-h = interpolate(5, ll, 10, hh, pres)
+h = interpolate(5, hl, 10, hh, pres)
 s = interpolate(5, sl, 10, sh, pres)
 ##### print results #####
-print('Properties at {} deg C and {} MPa are:'.format(pres, temp))
+print('Properties at {} deg C and {} MPa are:'.format(temp, pres))
 print('Specific volume (m^3/kg): {:.7f}'.format(v))
 print('Specific internal energy (kJ/kg): {:.2f}'.format(u))
 print('Specific enthalpy (kJ/kg): {:.2f}'.format(h))
