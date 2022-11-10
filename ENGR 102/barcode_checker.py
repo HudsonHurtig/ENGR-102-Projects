11768a7ff0307959112883f0a4fc14b856fabf67
Hudsonhurtig@yahoo.com <johndough1432@gmail.com>
2022-11-10 10:27:08 -0600

    added barcodes

---
+++ ENGR 102/barcode_checker.py
@@ -0,0 +1,43 @@
+# By submitting this assignment, I agree to the following:
+#   "Aggies do not lie, cheat, or steal, or tolerate those who do."
+#   "I have not given or received any unauthorized aid on this assignment."
+# Name:         Hudson Hurtig
+# Section:      575
+# Assignment:   lab 6
+# Date:         9-22-22
+
+#silly whittle comment
+
+def validateBarcode(string):
+       
+   intStr = [int(string[i]) for i in range(0,12)]
+   
+   sum1 = sum(intStr[::2])
+   sum2 = sum(intStr) - sum1
+   num3 = sum2*3
+   num4 = num3 + sum1
+   num5 = num4 % 10
+   num6 = 10 - num5
+   
+   return int(string[12]) == num6
+
+with open("barcodes.txt") as file:
+    counter = 0
+    bigString = file.read()
+    bigList = bigString.split("\n")
+    
+    for i in bigList:
+        
+        if validateBarcode(i):
+            
+            counter+=1
+            f = open("validbarcodes.txt", "a")
+            f.write(i+"\n")
+            f.close()
+        
+        print(i, validateBarcode(i))
+        
+    print(counter)
+    
+    
+
