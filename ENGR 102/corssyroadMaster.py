68b02da3f0c463e7b0c672cb2fa869f9a8afcde3
Hudsonhurtig@yahoo.com <johndough1432@gmail.com>
2022-12-04 17:22:58 -0600

    backup

--- ENGR 102/corssyroadMaster.py
+++ ENGR 102/corssyroadMaster.py
@@ -1,2 +1,3 @@
-import subprocess
-subprocess.call('python D:\Fall 2022 labs ENGR\ENGR 102\newestCorssyRoad.py', shell=True)
+import os
+
+os.system("start cmd /k C:/Python38/python.exe \"D:/Fall 2022 labs ENGR/ENGR 102/corssyroadMaster.py\"")
